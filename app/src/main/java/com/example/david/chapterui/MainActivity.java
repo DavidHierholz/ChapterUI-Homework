2ef79e49f94284ae2a8de6e282c0645a4a4738e1
kgudzius <kgudzius@gmail.com>
2018-02-27 15:41:11 +0100

    Testing Github

--- app/src/main/java/com/example/david/chapterui/MainActivity.java
+++ app/src/main/java/com/example/david/chapterui/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d("TEST","Testing github");
 
         drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
 
